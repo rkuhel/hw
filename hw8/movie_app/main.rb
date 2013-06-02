1b8953d3c82f9ff4e72c9e0b1e12800f830f34a6
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-06-02 11:24:05 -0400

    add, edit, delete, and project all movie data from psql db. Also, applied some bootstrap styling

--- hw8/movie_app/main.rb
+++ hw8/movie_app/main.rb
@@ -1,13 +1,19 @@
+require 'pg'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pry'
 
+##############################
+## home display add and new ##
+##############################
 
 get '/' do 
 	erb :home 
 end
 
 get '/display' do
+	sql = "select * from videos"
+	@rows = run_sql(sql)
 	erb :display
 end
 
@@ -17,23 +23,46 @@ get '/new' do
 end
 
 post '/add' do 
-	title = params[:title]
-	description = params[:description]
-	url = params[:url]
-	genre = params[:genre]
-	sql = "insert into videos values (title, description, url, genre), (#{params[:title]}, #{params[:description]}, #{url[:url]}, #{genre[:genre]})"
+	sql = "insert into videos (title, description, url, genre) values ( '#{params['title']}', '#{params['description']}', '#{params['url']}', '#{params['genre']}' )"
 	run_sql(sql)
-	redirect '/'
+	redirect to('/display')
 end 
 
-get '/edit' do
+###################
+## edit & delete ##
+###################
+
+get '/edit/:id' do 
+	sql = "select * from videos where id=#{params[:id]}"
+	@row = run_sql(sql).first
 	erb :edit
-end 
+end
 
+post '/update/:id' do 
+	sql = "update videos set title='#{params['title']}', description='#{params['description']}', url='#{params['url']}', genre='#{params['genre']}' where id= #{params['id']}"
+	@row = run_sql(sql).first
+	redirect to("/display")
+end
+
+get '/delete/:id' do  
+	sql = "delete * from videos where id=#{params[:id]}"
+	run_sql(sql)
+	redirect to('/display')
+end
+
+get '/video/:id/delete' do 
+	sql = "delete from videos where id=#{params[:id]}"
+	run_sql(sql)
+	redirect to('/display')
+end
+
+##########################
+## methods to run sql ###
+#########################
 
 def run_sql(sql)
 	conn = PG.connect(dbname: "video_store", host: 'localhost')
 	result = conn.exec(sql)
 	conn.close
-	result
+	result 
 end
